2d068af5a0a8b695b153b32ce08972a3dd6216f3
IsThatADonut <89513151+IsThatADonut@users.noreply.github.com>
2022-02-25 01:36:43 +0100

    Update stuff__hospital__resource.lua

--- stuff__hospital__resource.lua
+++ stuff__hospital__resource.lua
@@ -1,8 +1,8 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-description 'Mythic Framework Hospital & Damage System'
+description 'EMS framework for Ascension: Miami-dade Country RP'
 
-version '1.1.0'
+version '1.0'
 
 client_scripts {
 	'config.lua',

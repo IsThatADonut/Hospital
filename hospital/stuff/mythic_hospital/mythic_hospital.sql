3dac7868af600d64e32367db446567ec73b8f8fe
IsThatADonut <89513151+IsThatADonut@users.noreply.github.com>
2022-02-25 03:08:30 +0100

    Update mythic_hospital.sql

--- hospital/stuff/mythic_hospital/mythic_hospital.sql
+++ hospital/stuff/mythic_hospital/mythic_hospital.sql
@@ -1,15 +1,19 @@
-INSERT INTO `items` (name, label, `limit`) VALUES
-	('bandage','Bandage', 25),
-	('firstaid','First-Aid Kit', 10),
-	('valium','Valium', 5),
-	('fentanyl','Fentanyl', 5),
-	('morphine','Morphine', 5),
-        ('albuterol','Albuterol', 5)
-	('loratadine','Loratadine', 5),
-	('narcan','Narcan', 5),
-	('epinephrine','Epinephrine', 5),
-        ('bolus','Bolus', 5)
-	('adenosine','Adenosine', 5),
-	('asprin','Asprin', 5),
-	('ondansetron','Andansetron', 5),
+INSERT INTO `items` (`name`, `label`, `limit`, `weight`, `can_remove`, `illegal`) VALUES
+	('bandage','Bandage', 99, 0.5, 1, 1),
+	('firstaid','First-Aid Kit', 10, 5, 1, 1),
+	('valium','[[Valium, used for sedation and siezures]] ', 5, 0.1, 1, 1),
+	('fentanyl','[[Fentany, used for pain relief]]', 5, 0.1, 1, 1),
+	('morphine','[[Morphine, used for severe pain relief]]', 5, 0.1, 1, 1),
+        ('albuterol','[[Albuterol, used for asthma attacks]]', 5, 0.1, 1, 1)
+	('loratadine','[[Loratadine, allergic reactions]]', 5, 0.1, 1, 1),
+	('narcan','[[Narcan, used for drug overdoses]]', 5, 0.1, 1, 1),
+	('epinephrine','[[Epinephrine, used for quick relief]]', 5, 0.1, 1, 1),
+        ('bolus','[[Bolus, used for head injuries]]', 5, 0.1, 1, 1)
+	('adenosine','[[Adenosine, used for arrhythmia]]', 5, 0.1, 1, 1),
+	('asprin','[[Asprin, used for heart attack]]', 5, 0.1, 1, 1),
+	('ondansetron','[[Andansetron, used for nausea]]', 5, 0.1, 1, 1),
+;
+
+INSERT INTO `licenses` (`type`, `label`) VALUES
+	('paramedic_license', 'Paramedic License')
 ;

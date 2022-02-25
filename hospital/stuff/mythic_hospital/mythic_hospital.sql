4136d037041746b7950b9f0ba86a32f9046b39cd
IsThatADonut <89513151+IsThatADonut@users.noreply.github.com>
2022-02-25 02:39:31 +0100

    Update mythic_hospital.sql

--- hospital/stuff/mythic_hospital/mythic_hospital.sql
+++ hospital/stuff/mythic_hospital/mythic_hospital.sql
@@ -1,10 +1,15 @@
 INSERT INTO `items` (name, label, `limit`) VALUES
-	('gauze','Gauze', 25),
 	('bandage','Bandage', 25),
 	('firstaid','First-Aid Kit', 10),
-	('medkit','Medkit', 5),
-	('vicodin','Vicodin', 5),
-	('hydrocodone','Hydrocodone', 5),
-	('morphine','Morphine', 5),
-        ('adrenaline','Adrenaline', 5)
+	('valium','', 5),
+	('fentanyl','', 5),
+	('morphine','', 5),
+        ('albuterol','', 5)
+	('loratadine','', 5),
+	('narcan','', 5),
+	('epinephrine','', 5),
+        ('bolus','', 5)
+	('adenosine','', 5),
+	('asprin','', 5),
+	('ondansetron','', 5),
 ;
